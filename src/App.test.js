c8b4454035567995021603a0a4c1edf88191ce5e
oty87 <oty872020@gmail.com>
2021-05-01 11:22:18 +0300

    4

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,10 @@
+/* eslint-disable */
+
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
 test('renders learn react link', () => {
-  console.log('Hello!');
+  console.log('Hello!!!');
   render(<App />);
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
