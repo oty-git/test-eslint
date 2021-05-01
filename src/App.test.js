0b48166bf72d19f260712745020a5589a37d7aee
oty87 <oty872020@gmail.com>
2021-05-01 11:14:53 +0300

    change app.test

--- src/App.test.js
+++ src/App.test.js
@@ -2,7 +2,7 @@ import { render, screen } from '@testing-library/react';
 import App from './App';
 
 test('renders learn react link', () => {
-  console.log('Hi');
+  console.log('Hello!');
   render(<App />);
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
